a4e995a342c100c8deb554d357accdc0e1cd451a
Jaivin Mungara <71208031+jaivin92@users.noreply.github.com>
2023-10-25 11:41:17 +0530

    update image

--- src/component/MySkills.js
+++ src/component/MySkills.js
@@ -1,6 +1,27 @@
 import data from "../data/index.json"
+import skill1 from "../img/product-chain-1.png"
+import skill2 from "../img/tag-1.png"
+import skill3 from "../img/feather-pen-2.png"
+import skill4 from "../img/feather-pen-1.png"
 
 const MySkills = () => {
+
+  
+
+  const getImage = (index) => {
+    console.log("Hello world!", index)
+    
+      if(index ===0) {
+          return <img src={skill1} alt="Product Chain" />
+      } else if(index === 1) {
+        return <img src={skill2} alt="Product Chain" />
+      } else if(index === 2) {
+        return <img src={skill3} alt="Product Chain" />
+      } else if(index === 3) {
+        return <img src={skill4} alt="Product Chain" />
+      }
+        
+  }
     return (
         <section className="skills--section" id="mySkills">
           <div className="portfolio--container">
@@ -11,7 +32,8 @@ const MySkills = () => {
             {data?.skills?.map((item, index) => (
               <div key={index} className="skills--section--card">
                 <div className="skills--section--img">
-                  <img src={item.src} alt="Product Chain" />
+                  {/* <img src={item.src} alt="Product Chain" /> */}
+                  { getImage(index) }
                 </div>
                 <div className="skills--section--card--content">
                   <h3 className="skills--section--title">{item.title}</h3>
